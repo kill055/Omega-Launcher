646fe0cc8a83c0848a5ca6e379574dc88a5986a0
kill055 <twitch.kill032@gmail.com>
2026-02-10 12:00:50 +0100

    v1.0.30

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function initAutoUpdater(event, data) {
 ipcMain.on('autoUpdateAction', (event, arg, data) => {
     switch(arg){
         case 'initAutoUpdater':
-            console.log('Initializing auto updater.')
+            console.log('Inizializzazione dell\'aggiornamento automatico.')
             initAutoUpdater(event, data)
             event.sender.send('autoUpdateNotification', 'ready')
             break
@@ -80,7 +80,7 @@ ipcMain.on('autoUpdateAction', (event, arg, data) => {
             autoUpdater.quitAndInstall()
             break
         default:
-            console.log('Unknown argument', arg)
+            console.log('Argomento sconosciuto', arg)
             break
     }
 })
@@ -265,14 +265,14 @@ function createMenu() {
 
         // Extend default included application menu to continue support for quit keyboard shortcut
         let applicationSubMenu = {
-            label: 'Application',
+            label: 'Applicazione',
             submenu: [{
-                label: 'About Application',
+                label: 'Informazioni sull\'applicazione',
                 selector: 'orderFrontStandardAboutPanel:'
             }, {
                 type: 'separator'
             }, {
-                label: 'Quit',
+                label: 'Esci',
                 accelerator: 'Command+Q',
                 click: () => {
                     app.quit()
@@ -282,31 +282,31 @@ function createMenu() {
 
         // New edit menu adds support for text-editing keyboard shortcuts
         let editSubMenu = {
-            label: 'Edit',
+            label: 'Modifica',
             submenu: [{
-                label: 'Undo',
+                label: 'Disfare',
                 accelerator: 'CmdOrCtrl+Z',
                 selector: 'undo:'
             }, {
-                label: 'Redo',
+                label: 'Rifare',
                 accelerator: 'Shift+CmdOrCtrl+Z',
                 selector: 'redo:'
             }, {
                 type: 'separator'
             }, {
-                label: 'Cut',
+                label: 'Taglio',
                 accelerator: 'CmdOrCtrl+X',
                 selector: 'cut:'
             }, {
-                label: 'Copy',
+                label: 'Copia',
                 accelerator: 'CmdOrCtrl+C',
                 selector: 'copy:'
             }, {
-                label: 'Paste',
+                label: 'Incolla',
                 accelerator: 'CmdOrCtrl+V',
                 selector: 'paste:'
             }, {
-                label: 'Select All',
+                label: 'Seleziona tutto',
                 accelerator: 'CmdOrCtrl+A',
                 selector: 'selectAll:'
             }]
