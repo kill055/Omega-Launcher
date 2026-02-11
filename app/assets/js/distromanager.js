3efe64ace6a9ad6bb4133998abc0deeb0264916b
kill055 <twitch.kill032@gmail.com>
2026-02-11 21:30:34 +0100

    v1.0.50

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://panel.omegamc.it/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://panel.omegamc.it/server/9a0aba5f/files/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
